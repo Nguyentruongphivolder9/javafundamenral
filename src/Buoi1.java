66c8243687bba7da81c4344ced94b8889146e778
phi coder <ashuraitproz@gmai.com>
2022-03-02 20:47:23 +0700

    1 : kiểu dữ liệu

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -4,6 +4,14 @@ public class Buoi1 {
 
     public static void main(String[] args) {
 
-        
+        // 1 : kiểu dữ liệu
+        // Nhóm 1 : kiểu dữ liệu nguyên thủy
+        // String (chuỗi)
+        // int, long ( kiểu số nguyên )
+        // float, double ( kiểu số thực hay gọi là số có thập phân )
+        // boolean ( kiểu dữ liệu chỉ 2 miền giá trị đúng or sai )
+
+        // Nhóm 2 : kiểu dữ liệu đối tượng ( object type )
+
     }
 }
